376f70dcb57a42cf069e6be91c0804d6087e896d
Sander Peguero <sanderpeguero.21@gmail.com>
2022-11-14 19:26:46 -0400

    Navigation TopBar and Drawer

--- QuantumSwapApp/app/src/main/java/edu/ucne/quantumswap/MainActivity.kt
+++ QuantumSwapApp/app/src/main/java/edu/ucne/quantumswap/MainActivity.kt
@@ -4,29 +4,59 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.quantumswap.ui.components.drawer
+import edu.ucne.quantumswap.ui.components.topBar
+import edu.ucne.quantumswap.ui.navigation.Destinations
+import edu.ucne.quantumswap.ui.navigation.navigationHost
 import edu.ucne.quantumswap.ui.theme.QuantumSwapTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuantumSwapTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
+//            QuantumSwapTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+){
+    val navController = rememberNavController()
+    val scaffoldState = rememberScaffoldState(
+        drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    )
+    val scope = rememberCoroutineScope()
+    val navigationItems = listOf(
+        Destinations.Home
+    )
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { topBar(scope, scaffoldState) },
+        drawerContent = { drawer(scope, scaffoldState, navController, items = navigationItems) },
+        drawerGesturesEnabled = true
+    ) {
+        navigationHost(navController)
+    }
+
 }
 
 @Composable
---
+++ QuantumSwapApp/app/src/main/java/edu/ucne/quantumswap/ui/navigation/Destinations.kt
@@ -0,0 +1,13 @@
+package edu.ucne.quantumswap.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Destinations(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+){
+    object Home: Destinations("Home", "Home", Icons.Filled.Home)
+}

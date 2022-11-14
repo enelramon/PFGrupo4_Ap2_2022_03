7bb32976037dd6725b28e9f35ba33366795c4292
jose22gonzalez <97250949+jose22gonzalez@users.noreply.github.com>
2022-11-14 17:27:02 -0400

    Add Home

--- QuantumSwapApp/app/src/main/java/edu/ucne/quantumswap/MainActivity.kt
+++ QuantumSwapApp/app/src/main/java/edu/ucne/quantumswap/MainActivity.kt
@@ -1,43 +1,32 @@
 package edu.ucne.quantumswap
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.quantumswap.ui.Home.HomeScreen
 import edu.ucne.quantumswap.ui.theme.QuantumSwapTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QuantumSwapTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold (
+                    topBar = {CenterAlignedTopAppBar(title = {Text("Quantum Swap")})
+                    },
+                ){
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    QuantumSwapTheme {
-        Greeting("Android")
-    }
-}
